448ab3128a2b92d256a5deaa26bab3a95d8a8c22
MoguchiyDuh <kirnekrasov12@gmail.com>
2026-03-01 09:02:52 +0100

    str_reverse.c done

--- c/src/str_reverse.c
+++ c/src/str_reverse.c
@@ -1,25 +1,7 @@
-#include <stddef.h>
-#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-void str_reverse_alloc(char *str) {
-  if (str == NULL || *str == '\0')
-    return;
-
-  size_t len = strlen(str);
-  char str_reversed[len + 1]; // \0
-
-  for (size_t i = 0; i < len; ++i) {
-    str_reversed[i] = str[len - 1 - i];
-  }
-
-  str_reversed[len] = '\0';
-  strcpy(str, str_reversed);
-}
-
-void str_reverse_no_alloc(char *str) {
+void str_reverse(char *str) {
   if (str == NULL || *str == '\0')
     return;
 
@@ -36,8 +18,6 @@ void str_reverse_no_alloc(char *str) {
 int main(void) {
   char string1[] = "hello";
   printf("%s\n", string1);
-  str_reverse_alloc(string1);
-  printf("%s\n", string1);
-  str_reverse_no_alloc(string1);
+  str_reverse(string1);
   printf("%s\n", string1);
 }
